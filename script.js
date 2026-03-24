b13f76373555302712d6283d678a9743f22bdf1e
kkkk-ashen <adaguna28@gmail.com>
2026-03-24 09:39:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const projectData = {
-    "HIBA": "HIBA is a 3D Survival Game inspired by <b>muck</b>, Where you born with nothing and start gathering recources such as wood, metal ingot and many more to make tools from it to defeat bosses and beat the game. well thats the plan though, for now i only done making procedural generations(meaning: you would not get the same world as you play.), and spawning a pretty boring grass and trees so you can cut 'em down."
+    "HIBA": "HIBA is a 3D Survival Game inspired by <b>Muck</b>, Where you born with nothing and start gathering recources such as wood, metal ingot and many more to make tools from it to defeat bosses and beat the game. well thats the plan though, for now i only done making procedural generations(meaning: you would not get the same world as you play.), and spawning a pretty boring grass and trees so you can cut 'em down."
 };
 
 const modal = document.getElementById("projectModal");
