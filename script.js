53810f35ac197575ca4834109e01bd7f69f4fabc
kkkk-ashen <adaguna28@gmail.com>
2026-04-07 20:47:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -222,3 +222,15 @@ document.querySelectorAll('img').forEach(img => {
         return false;
     });
 });
+
+window.addEventListener('wheel', (e) => {
+    if (e.ctrlKey) {
+        e.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && (e.key === '+' || e.key === '=' || e.key === '-' || e.key === '0')) {
+        e.preventDefault();
+    }
+});
